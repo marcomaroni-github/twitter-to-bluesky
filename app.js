ddb8549982e8dd6a4f9e5b8079f94b4f3adf04cd
Marco Maroni <marcomaroni@gmail.com>
2024-04-08 14:38:11 +0200

    hashtag

--- app.js
+++ app.js
@@ -6,78 +6,104 @@ const sConfig = FS.readFileSync("config.json");
 const CONFIG = JSON.parse(sConfig);
 
 async function CreateRecord(bearerToken, tweet) {
-   let options = {
-      'method': 'POST',
-      'hostname': 'bsky.social',
-      'path': '/xrpc/com.atproto.repo.createRecord',
-      'headers': {
-         'Authorization': 'Bearer ' + bearerToken,
-         'Content-Type': 'application/json'
-      },
-      'maxRedirects': 20
-   };
-   
-   const reqCreateRecord = HTTPS.request(options, (res) => {
-      let chunks = [];
-   
-      res.on("data", (chunk) => {
-         chunks.push(chunk);
-      });
-   
-      res.on("end", (chunk) => {
-         let body = Buffer.concat(chunks);
-         const recordData = JSON.parse(body.toString());
-         const i = recordData.uri.lastIndexOf("/");
-         if( i>0 ) {
-            const rkey = recordData.uri.substring(i+1);
-            const postUri = `https://bsky.app/profile/${CONFIG.bskyAccount}/post/${rkey}`;
-            console.log("  Blusky post create, URI: " + postUri);
-         }
-         else {
-            console.log(recordData);
-         }
-      });
-   
-      res.on("error", (error) => {
-         console.error(error);
-      });
-   });
+  let options = {
+    method: "POST",
+    hostname: "bsky.social",
+    path: "/xrpc/com.atproto.repo.createRecord",
+    headers: {
+      Authorization: "Bearer " + bearerToken,
+      "Content-Type": "application/json",
+    },
+    maxRedirects: 20,
+  };
 
-   let facets = [];
-    URI.withinString(tweet.full_text, (url, start, end)=> {
-      const facet = {
-         index : {
-            byteStart: start,
-            byteEnd: end
-         },
-         "features": [
-            {
-               "$type": "app.bsky.richtext.facet#link",
-               "uri": url
-            }
-         ]
+  const reqCreateRecord = HTTPS.request(options, (res) => {
+    let chunks = [];
+
+    res.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    res.on("end", (chunk) => {
+      let body = Buffer.concat(chunks);
+      const recordData = JSON.parse(body.toString());
+      const i = recordData.uri.lastIndexOf("/");
+      if (i > 0) {
+        const rkey = recordData.uri.substring(i + 1);
+        const postUri = `https://bsky.app/profile/${CONFIG.bskyAccount}/post/${rkey}`;
+        console.log("  Blusky post create, URI: " + postUri);
+      } else {
+        console.log(recordData);
       }
+    });
 
-      facets.push(facet);
+    res.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  let facets = [];
+  { // links
+    URI.withinString(tweet.full_text, (url, start, end) => {
+      facets.push({
+        index: {
+          byteStart: start,
+          byteEnd: end,
+        },
+        features: [
+          {
+            $type: "app.bsky.richtext.facet#link",
+            uri: url,
+          },
+        ],
+      });
 
       return url;
-   });
+    });
+  }
 
-   let postData = JSON.stringify({
-      "repo": CONFIG.bskyAccount,
-      "collection": "app.bsky.feed.post",
-      "record": {
-         "text": tweet.full_text,
-         "facets": facets,
-         "createdAt": new Date(tweet.created_at).toISOString()
-      }
-   });
-   
-   reqCreateRecord.write(postData);
-   
-   reqCreateRecord.end();   
+  { // hashtag
+    const re = /(?:^|\s)(#[^\d\s]\S*)(?=\s)?/g;
+    while ((match = re.exec(tweet.full_text))) {
+      let [tag] = match;
+      const hasLeadingSpace = /^\s/.test(tag);
+
+      tag = tag.trim().replace(/\p{P}+$/gu, ""); // strip ending punctuation
+
+      // inclusive of #, max of 64 chars
+      if (tag.length > 66) continue;
+
+      const index = match.index + (hasLeadingSpace ? 1 : 0);
+
+      facets.push({
+        index: {
+          byteStart: index,
+          byteEnd: index + tag.length, // inclusive of last char
+        },
+        features: [
+          {
+            $type: "app.bsky.richtext.facet#tag",
+            tag: tag.replace(/^#/, ""),
+          },
+        ],
+      });
+    }
+  }
+
+  let postData = JSON.stringify({
+    repo: CONFIG.bskyAccount,
+    collection: "app.bsky.feed.post",
+    record: {
+      text: tweet.full_text,
+      facets: facets,
+      createdAt: new Date(tweet.created_at).toISOString(),
+    },
+  });
+
+  reqCreateRecord.write(postData);
+
+  reqCreateRecord.end();
 }
-
 
 function GetSession(identifier, password) {
   let options = {
@@ -101,55 +127,52 @@ function GetSession(identifier, password) {
       var body = Buffer.concat(chunks);
       // console.log(body.toString());
       sessionData = JSON.parse(body.toString());
-      if( sessionData?.accessJwt != null ) {
-         const bearerToken = sessionData?.accessJwt;
-         console.log("ACCESS_JWT:" + bearerToken);
+      if (sessionData?.accessJwt != null) {
+        const bearerToken = sessionData?.accessJwt;
+        console.log("ACCESS_JWT:" + bearerToken);
 
-         const fTweets = FS.readFileSync(CONFIG.tweetsFile);
-         const tweets = JSON.parse(fTweets);
-         if (tweets != null && tweets.length > 0) {
-            const sortedTweets = tweets.sort((a,b) => {
-               ad = new Date(a.tweet.created_at).getTime();
-               bd = new Date(b.tweet.created_at).getTime()
-               return  ad - bd;
+        const fTweets = FS.readFileSync(CONFIG.tweetsFile);
+        const tweets = JSON.parse(fTweets);
+        if (tweets != null && tweets.length > 0) {
+          const sortedTweets = tweets.sort((a, b) => {
+            ad = new Date(a.tweet.created_at).getTime();
+            bd = new Date(b.tweet.created_at).getTime();
+            return ad - bd;
+          });
+
+          // const first = sortedTweets[0].tweet;
+          // const last = sortedTweets[sortedTweets.length-1].tweet
+
+          for (let index = 0; index < sortedTweets.length; index++) {
+            const tweet = sortedTweets[index].tweet;
+
+            if (tweet.id != "1586630236405469189") continue;
+
+            console.log(`Parse tweet id '${tweet.id}'`);
+            console.log(` Created at ${new Date(tweet.created_at).toISOString()}`);
+            console.log(` Full text '${tweet.full_text}'`);
+            URI.withinString(tweet.full_text, (url) => {
+              console.log(` Inner URL '${url}'`);
             });
 
-            // const first = sortedTweets[0].tweet;
-            // const last = sortedTweets[sortedTweets.length-1].tweet
-           
-            for (let index = 0; index < sortedTweets.length; index++) {
-               const tweet = sortedTweets[index].tweet;
-
-               if( tweet.id != "890168203791536128")
-                  continue;
-
-               console.log(`Parse tweet id '${tweet.id}'`)
-               console.log(` Created at ${new Date(tweet.created_at).toISOString()}`);
-               console.log(` Full text '${tweet.full_text}'`);
-               URI.withinString(tweet.full_text, (url)=> {
-                  console.log(` Inner URL '${url}'`);
-               });
-            
-               if( tweet.in_reply_to_screen_name ) {
-                  console.log("Discarded (reply)");
-                  continue
-               }
-               if( tweet.full_text.startsWith("@") ) {
-                  console.log("Discarded (start with @)");
-                  continue
-               }
-               if( tweet.full_text.startsWith("RT ") ) {
-                  console.log("Discarded (start with RT)");
-                  continue
-               }
-
-               CreateRecord(bearerToken, tweet);
-
-               if (index > 1)
-                  break;
+            if (tweet.in_reply_to_screen_name) {
+              console.log("Discarded (reply)");
+              continue;
             }
-         }
-       
+            if (tweet.full_text.startsWith("@")) {
+              console.log("Discarded (start with @)");
+              continue;
+            }
+            if (tweet.full_text.startsWith("RT ")) {
+              console.log("Discarded (start with RT)");
+              continue;
+            }
+
+            CreateRecord(bearerToken, tweet);
+
+            if (index > 1) break;
+          }
+        }
       }
     });
 
@@ -167,7 +190,5 @@ function GetSession(identifier, password) {
 
   req.end();
 }
-
-
 
 GetSession(CONFIG.bskyAccount, CONFIG.bskyPwd);
