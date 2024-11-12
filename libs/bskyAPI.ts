56729a2a01bc80524dcc74a6cffa367c80e4ac85
Clemens Vasters <clemens@vasters.com>
2024-11-12 09:41:51 +0100

    command line args support

--- libs/bskyAPI.ts
+++ libs/bskyAPI.ts
@@ -1,23 +1,8 @@
-import * as dotenv from 'dotenv';
-import * as process from 'process';
 import FS from 'fs';
-
-dotenv.config();
 
 export const TWEETS_MAPPING_FILE_NAME = 'tweets_mapping.json'; // store the imported tweets & bsky id mapping
 
-
-let MIN_DATE: Date | undefined = undefined;
-if (process.env.MIN_DATE != null && process.env.MIN_DATE.length > 0)
-    MIN_DATE = new Date(process.env.MIN_DATE as string);
-
-let MAX_DATE: Date | undefined = undefined;
-if (process.env.MAX_DATE != null && process.env.MAX_DATE.length > 0)
-    MAX_DATE = new Date(process.env.MAX_DATE as string);
-
-
-
-export async function deleteBskyPosts(agent, tweets){
+export async function deleteBskyPosts(agent, tweets, minDate: Date, maxDate: Date){
 // Delete bsky posts with a record in TWEETS_MAPPING_FILE_NAME.
 // If something goes wrong, call this method to clear the previously imported posts.
 // You may also use MIN_DATE and MAX_DATE to limit the range.
@@ -30,9 +15,9 @@ export async function deleteBskyPosts(agent, tweets){
               
                 const tweetDate = new Date(tweet.created_at);
       
-                if (MIN_DATE != undefined && tweetDate < MIN_DATE)
+                if (minDate != undefined && tweetDate < minDate)
                     continue;
-                if (MAX_DATE != undefined && tweetDate > MAX_DATE)
+                if (maxDate != undefined && tweetDate > maxDate)
                     continue;
                 
                 await agent.deletePost(bsky.uri);
